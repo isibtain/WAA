2e64d52ab589fc9ab7c77a550a87f45679ce9f31
Sibtain Raza <47073093+isibtain@users.noreply.github.com>
2020-01-27 22:05:06 -0600

    Working on Lab9

--- Lab9_LoginFlow/src/main/java/edu/mum/service/impl/LoginServiceImpl.java
+++ Lab9_LoginFlow/src/main/java/edu/mum/service/impl/LoginServiceImpl.java
@@ -6,20 +6,15 @@ import edu.mum.domain.Login;
 import edu.mum.service.LoginService;
 
 @Service
-public class LoginServiceImpl implements LoginService
-{
-		public String verify(Login login)
-		{
-				String userName = login.getUserName();
-				String password = login.getPassword();
-				if(userName.equals("admin") && password.equals("admin"))
-				{
-						return "true";
-				}
-				else
-				{
-						return "false";
-				}
+public class LoginServiceImpl implements LoginService {
+	public String verify(Login login) {
+		String userName = login.getUserName();
+		String password = login.getPassword();
+		if (userName.equals("admin") && password.equals("admin")) {
+			return "true";
+		} else {
+			return "false";
 		}
-		
+	}
+
 }
